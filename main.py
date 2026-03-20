a4e3b4dfa4add0b3601101a1caa6e1c15f299c1d
atsitimolan <atsitimolan@gmail.com>
2026-03-20 09:35:57 +0200

    Added metadata and better output

--- main.py
+++ main.py
@@ -10,11 +10,20 @@ if api_key == None:
 
 client = genai.Client(api_key=api_key)
 model = "gemini-2.5-flash"
+user_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
+print(f"User prompt: {user_prompt}")
 
 response = client.models.generate_content(
-    model=model, contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    model=model, contents=user_prompt
 )
-print(response.text)
+
+if (response.usage_metadata == None):
+    raise RuntimeError("No usage metadata.")
+else:
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+print(f"Response: \n{response.text}")
 
 def main():
     print("Hello from aigent!")
