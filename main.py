9320cde25c2a15c1b3f33df3ebfb0ade69b28dd7
atsitimolan <atsitimolan@gmail.com>
2026-03-22 14:00:14 +0200

    added genai types

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os, argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
@@ -15,12 +16,13 @@ def main():
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
     args = parser.parse_args()
-    user_prompt = args.user_prompt
 
-    print(f"User prompt: {user_prompt}")
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+    print(f"User prompt: {messages}")
 
     response = client.models.generate_content(
-        model=model, contents=user_prompt
+        model=model, contents=messages
     )
 
     if (response.usage_metadata == None):
